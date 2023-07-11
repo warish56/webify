9a19e28cccba2dba408cd6431f1344f37093fb43
warish-56 <warish@grexit.com>
2023-07-11 23:39:14 +0530

    service section added

---
+++ src/constants/home.ts
@@ -0,0 +1,6 @@
+
+export const serviceDescription = {
+    web: 'Website and ecommerce do not have to be expensive complicated in order to be effective. we deal with a wide spectrum of clients with varying budgets and needs.',
+    webApp: 'We provide web applications on variety of open source platforms to enable optimal budget management.',
+    mobile: 'Website and ecommerce do not have to be expensive complicated in order to be effective. we deal with a wide spectrum of clients with varying budgets and needs.'
+}
